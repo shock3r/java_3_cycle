15a75ed677b132995ab0275d8ae1a66886bbe0b4
shock3r <51910937+shock3r@users.noreply.github.com>
2019-07-05 16:31:33 +0300

    Add files via upload

---
+++ by/epam/cycle/task6/Main.java
@@ -0,0 +1,17 @@
+package by.epam.cycle.task6;
+public class Main {
+	
+	public static void main(String[] args) {
+		/*
+		 * Вывести на экран соответствий между символами и их численными обозначениями в памяти компьютера.
+		 */
+		
+		for (int i=0; i<255; i++) {
+			
+			System.out.println("ascii code: " + i + " is char: " + (char)i);
+			
+		}
+					
+	}
+
+}
